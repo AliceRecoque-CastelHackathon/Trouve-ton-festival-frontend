34b17e0cb0a021edbec3365dc96e07f54ca90913
Angelique Priam <angelique.priam@hotmail.com>
2023-10-12 15:01:08 +0200

    Map display okay

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_KEY: 'AIzaSyBEyYIZ9_WiM0eobvrggx4q9k0BW2dqbR4',
+  },
   images: {
     remotePatterns: [
       {
-          protocol: 'http',
-          hostname: '**',
-          port: '',
-          pathname: '**',
+        protocol: 'http',
+        hostname: '**',
+        port: '',
+        pathname: '**',
       },
     ],
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/festival/page.tsx
+++ src/app/festival/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -10,8 +10,31 @@ import Typography from '@mui/material/Typography';
 import Link from 'next/link';
 import festivals from '../../mockupData.json';
 import Image, { ImageLoader } from 'next/image';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { Stack } from '@mui/material';
 
 export default function FestivalList() {
+  const libraries = useMemo(() => ['places'], []);
+  const [geoPosX, setGeoPosX] = useState(0);
+  const [geoPosY, setGeoPosY] = useState(0);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+    libraries: libraries as any,
+  });
+  const mapCenter = useState({ lat: geoPosX, lng: geoPosY });
+
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    [],
+  );
+  const imageLoader: ImageLoader = ({ src }) => {
+    return `http://localhost:3003/${src}`;
+  };
+
   //   const [festivalArray, setFestivalArray] = useState<FestivalGetDto[]>([]);
   //   const [error, setError] = useState(null);
   //   const [loading, setLoading] = useState(true);
@@ -30,51 +53,66 @@ export default function FestivalList() {
   //       setLoading(true);
   //     }
   //   };
-  const imageLoader: ImageLoader = ({ src }) => {
-    return `http://localhost:3003/${src}`;
-  };
-  return (
-    <>
-      {festivals.results?.map((festival, index: number) => {
-        return (
-          <Card
-            key={index}
-            sx={{
-              marginTop: 2,
-              marginLeft: 2,
-              borderRadius: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Festival
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Nom: {festival.nom_du_festival}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lieu: {festival.adresse_postale}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Organisateur : {festival.libelle_epci_collage_en_valeur}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Link
-                href={'./../festival/' + festival.id}
-                style={{ textDecoration: 'none', margin: 'auto' }}
+  if (!isLoaded) return <div>Chargement...</div>;
+  else
+    return (
+      <>
+        {festivals.results?.map((festival, index: number) => {
+          const handleClickOnMap = (
+            event: React.MouseEvent<HTMLDivElement>,
+          ) => {
+            setGeoPosX(festival.geocodage_xy.lon);
+            setGeoPosY(festival.geocodage_xy.lat);
+          };
+          return (
+            <Stack direction={'row-reverse'}>
+              <GoogleMap
+                options={mapOptions}
+                zoom={14}
+                center={{ lat: mapCenter[0].lat, lng: mapCenter[0].lng }}
+                mapTypeId={google.maps.MapTypeId.ROADMAP}
+                mapContainerStyle={{ width: '800px', height: '800px' }}
+                onLoad={() => console.log('Map Component Loaded...')}
+              ></GoogleMap>
+              <Card
+                key={index}
+                sx={{
+                  marginTop: 2,
+                  marginLeft: 2,
+                  borderRadius: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={handleClickOnMap}
               >
-                Details
-              </Link>
-            </CardActions>
-          </Card>
-        );
-      })}
-      
-    </>
-  );
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Festival
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Nom: {festival.nom_du_festival}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lieu: {festival.adresse_postale}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Organisateur : {festival.libelle_epci_collage_en_valeur}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Link
+                    href={'./../festival/' + festival.id}
+                    style={{ textDecoration: 'none', margin: 'auto' }}
+                  >
+                    Details
+                  </Link>
+                </CardActions>
+              </Card>
+            </Stack>
+          );
+        })}
+      </>
+    );
 }
